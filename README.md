b76cb6c813da0a7999d0c6688db5e24405639d33
osx user <gili.shemesh@gmail.com>
2018-12-10 13:29:33 +0200

    deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+# v0.010 theremini-online  http://shemeshg.github.io/theremini-online
---
+++ precache-manifest.8814114460a51811c3985537accec437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/theremini-online/robots.txt"
+  },
+  {
+    "revision": "03d7049bb5fe2eb94bed",
+    "url": "/theremini-online/js/chunk-vendors.1a244d5a.js"
+  },
+  {
+    "revision": "daaca14f70b420950d9c",
+    "url": "/theremini-online/js/app.ffe5d19c.js"
+  },
+  {
+    "revision": "e7c2b3851ab3965b1ed8",
+    "url": "/theremini-online/js/about.b8cabc56.js"
+  },
+  {
+    "revision": "c4ae05e206c425888b4482b111908d61",
+    "url": "/theremini-online/index.html"
+  },
+  {
+    "revision": "03d7049bb5fe2eb94bed",
+    "url": "/theremini-online/css/chunk-vendors.1d759409.css"
+  },
+  {
+    "revision": "daaca14f70b420950d9c",
+    "url": "/theremini-online/css/app.6127fdb7.css"
+  }
+];
